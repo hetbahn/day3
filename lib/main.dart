bea934a3075975735e7cdfcacfe23a25b9416263
summeracne <summeracne@gmail.com>
2021-01-28 14:19:23 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:day3/pages/StarterPage.dart';
+
+void main() => runApp(MaterialApp(
+      theme: ThemeData(),
+      home: StarterPage(),
+    ));
---
+++ lib/pages/StarterPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StarterPage extends StatefulWidget {
+  @override
+  _StarterPageState createState() => _StarterPageState();
+}
+
+class _StarterPageState extends State<StarterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        color: Colors.yellow,
+      ),
+    );
+  }
+}
