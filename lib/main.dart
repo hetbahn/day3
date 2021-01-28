ee9031b6cc76c36e72b343ead57061bde9614b5e
summeracne <summeracne@gmail.com>
2021-01-28 15:27:44 +0900

    메인화면

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:day3/pages/StarterPage.dart';
 
-void main() => runApp(MaterialApp(
-      theme: ThemeData(),
-      home: StarterPage(),
-    ));
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(fontFamily: 'Roboto'),
+    home: StarterPage(),
+  )
+);
--- lib/pages/StarterPage.dart
+++ lib/pages/StarterPage.dart
@@ -8,9 +8,73 @@ class StarterPage extends StatefulWidget {
 class _StarterPageState extends State<StarterPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        color: Colors.yellow,
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/starter-image.jpg'),
+                fit: BoxFit.cover)),
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(begin: Alignment.bottomCenter, colors: [
+            Colors.black.withOpacity(.9),
+            Colors.black.withOpacity(.8),
+            Colors.black.withOpacity(.2),
+          ])),
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Text(
+                  'Taking Order For Deloivery',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "See resturants nearby by \nadding location",
+                  style:
+                      TextStyle(color: Colors.white, height: 1.4, fontSize: 18),
+                ),
+                SizedBox(
+                  height: 100,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      gradient: LinearGradient(
+                          colors: [Colors.yellow, Colors.orange])),
+                  child: MaterialButton(
+                    onPressed: () {},
+                    minWidth: double.infinity,
+                    child: Text(
+                      "Start",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Align(
+                  child: Text(
+                    "Now Deliver To Your Door 24/7",
+                    style: TextStyle(color: Colors.white70, fontSize: 15),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
